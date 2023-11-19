43d5dbfea913d1007ee51df4e050ecb21be1ad0f
aldenfachetti <aldenmerlin@gmail.com>
2023-11-19 18:25:20 +0100

    Using Storybook Controls and generating a static build

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,20 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'Boilerplate NextJS',
+  description = 'TypeScript, ReactJS, NextJS e Styled Components'
+}) => (
   <S.Wrapper>
-    <h1>Boilerplate NextJS</h1>
+    <S.Logo
+      src="/img/logo-white.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+    <S.Title>{title}</S.Title>
+    <S.Description>{description}</S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Um desenvolvedor de frente para uma tela com código."
+    />
   </S.Wrapper>
 )
 
